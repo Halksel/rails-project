c3fc994fbb780afacb33a4d72e2218a35308b46d
平健太郎 <whentheycry0708@gmail.com>
2019-03-12 17:45:44 +0900

    finish chapter-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts , :only => [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :welcome , :only => [:index]
+  root "welcome#index"
+
+  get '/welcome' => "welcome#index"
 end
